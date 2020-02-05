c54c92bb4086946b72cc5efe79524ebba5ea759c
Antoine <pezetantoine.pro@gmail.com>
2020-02-05 17:37:41 +0100

    Redux implementation, Modal component add

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar'
+import ShoppingList from './components/ShoppingList'
+import ItemModal from './components/ItemModal'
+import { Container } from 'reactstrap'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-import AppNavbar from './components/appNavbar'
 
-function App() {
-  return (
-    <div className="App">
-      <AppNavbar />
-    </div>
-  );
+import { Provider } from 'react-redux'
+import store from './store'
+
+class App extends Component {
+  render(){
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
